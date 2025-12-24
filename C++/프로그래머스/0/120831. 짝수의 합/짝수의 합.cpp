8931cf6c118f82c804381e9c1b92302d6261e5e8
yahoo-afk <sjffhdfg@gmail.com>
2025-12-24 09:31:40 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int sum = 0;
+    for (int i =0 ; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i  ;
+        }
+    }
+    return sum;
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << solution(n) << endl;
+}
